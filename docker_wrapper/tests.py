c4605b3325d8a71302274b064ff2d94c14d00e87
James Adams <james.adams@stfc.ac.uk>
2021-04-26 14:31:36 +0100

    Add expected job arguments to test resources
    i.e. First line is input, second line is expected output

--- docker_wrapper/tests.py
+++ docker_wrapper/tests.py
@@ -8,16 +8,22 @@ import docker
 
 class TestDockerWrapper(unittest.TestCase):
     def setUp(self):
-        self.cms_job_args = open('test_resources/cms_job.txt').read().split()
-        self.atlas_job_args = open('test_resources/atlas_job.txt').read().split()
+        self.maxDiff = None
+        self.atlas_job_args_before, self.atlas_job_args_after = [
+            l.split() for l in open('test_resources/atlas_job.txt').readlines()
+        ]
+        self.cms_job_args_before, self.cms_job_args_after = [
+            l.split() for l in open('test_resources/cms_job.txt').readlines()
+        ]
 
     def test_gateway(self):
         self.assertFalse(docker.gateway([]))
         self.assertFalse(docker.gateway(['']))
         self.assertFalse(docker.gateway(['version']))
 
-        self.assertTrue(docker.gateway(self.cms_job_args))
-        self.assertTrue(docker.gateway(self.atlas_job_args))
+        self.assertTrue(docker.gateway(self.atlas_job_args_before))
+        self.assertTrue(docker.gateway(self.cms_job_args_before))
+
 
 
 if __name__ == '__main__':
