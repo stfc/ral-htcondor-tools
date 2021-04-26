a95b780a88f5c92def8259ab70d511bb45d48003
James Adams <james.adams@stfc.ac.uk>
2021-04-26 14:33:59 +0100

    Test args_create with a mocked getfqdn

--- docker_wrapper/tests.py
+++ docker_wrapper/tests.py
@@ -3,6 +3,8 @@
 
 import unittest
 
+from mock import patch
+
 import docker
 
 
@@ -16,6 +18,9 @@ class TestDockerWrapper(unittest.TestCase):
             l.split() for l in open('test_resources/cms_job.txt').readlines()
         ]
 
+    def mocked_getfqdn(self):
+        return 'host.example.org'
+
     def test_gateway(self):
         self.assertFalse(docker.gateway([]))
         self.assertFalse(docker.gateway(['']))
@@ -23,6 +28,28 @@ class TestDockerWrapper(unittest.TestCase):
 
         self.assertTrue(docker.gateway(self.atlas_job_args_before))
         self.assertTrue(docker.gateway(self.cms_job_args_before))
+
+    def test_args_create(self):
+        basic_args = [
+            '-eSINGULARITY_BINDPATH=/etc/hosts',
+            '--cap-add=SYS_ADMIN',
+            '--cap-add=DAC_OVERRIDE',
+            '--cap-add=SETUID',
+            '--cap-add=SETGID',
+            '--cap-add=SYS_CHROOT',
+            '--env=SINGULARITYENV_FRONTIER_LOG_FILE=frontier.log',
+            '--env=PANDA_HOSTNAME=host.example.org',
+            '--env=SINGULARITYENV_PANDA_HOSTNAME=host.example.org',
+            '--env=PILOT_NOKILL=1',
+        ]
+        with patch('docker.getfqdn') as mock_socket:
+            mock_socket.return_value = 'host.example.org'
+
+            self.assertEqual(docker.args_create([]), basic_args + ['--label=xrootd-local-gateway=false'])
+            self.assertEqual(docker.args_create(['']), basic_args + ['--label=xrootd-local-gateway=false', ''])
+
+            self.assertEqual(docker.args_create(self.atlas_job_args_before), self.atlas_job_args_after)
+            self.assertEqual(docker.args_create(self.cms_job_args_before), self.cms_job_args_after)
 
 
 
