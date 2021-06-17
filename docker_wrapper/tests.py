5cc73b8418a2d021af0c2af78081468027e79825
James Adams <james.adams@stfc.ac.uk>
2021-06-17 15:10:36 +0100

    Allow tests to be called from any directory

--- docker_wrapper/tests.py
+++ docker_wrapper/tests.py
@@ -10,6 +10,7 @@ except ImportError:
 
 from random import choice, randint
 from string import ascii_letters
+from os.path import dirname, join
 
 import docker
 
@@ -17,11 +18,12 @@ import docker
 class TestDockerWrapper(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
+        self.resource_dir = join(dirname(__file__), 'test_resources')
         self.atlas_job_args_before, self.atlas_job_args_after = [
-            l.split() for l in open('test_resources/atlas_job.txt').readlines()
+            l.split() for l in open(join(self.resource_dir, 'atlas_job.txt')).readlines()
         ]
         self.cms_job_args_before, self.cms_job_args_after = [
-            l.split() for l in open('test_resources/cms_job.txt').readlines()
+            l.split() for l in open(join(self.resource_dir, 'cms_job.txt')).readlines()
         ]
 
     def mocked_getfqdn(self):
