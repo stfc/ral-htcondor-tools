33ebcf213e9f7b2c22562b60526fa285191ae1e6
Thomas Birkett <thomas.birkett@stfc.ac.uk>
2023-02-24 10:28:57 +0000

    Remove old Singularity 2 config. Replace singularity references with apptainer With singualrity support being phased out, singularity 2 is considered obsolete.

--- docker.py
+++ docker.py
@@ -13,16 +13,13 @@ def gateway():
     return gateway_needed and not os.path.isfile('/etc/nogateway')
 
 def args_create(argv):
-    # Define major Singularity version
-    singularity_major_version = int(3)
-
     """
     Build the new list of command line arguments for command
         docker create
     """
     dargs = []
 
-    # Allow singularity to bind local hosts file
+    # Allow apptainer to bind local hosts file
     dargs.append('--env=APPTAINER_BINDPATH=/etc/hosts')
 
     # ATLAS fix for 21.0.XX release errors with frontier
@@ -40,34 +37,18 @@ def args_create(argv):
     # It may trigger a SIGKILL signal to the pilot.
     dargs.append('--env=PILOT_NOKILL=1')
 
-    # For older Singularity versions we must define Docker capabilities.
-    # Newer Singularity versions rely on security-options
-    # This checks which image we are using and sets Docker create options accordingly
-    for arg in argv:
-        # Check for old image containing Singularity 2
-        if 'grid-workernode' in arg and arg.endswith(':2019-07-02.1'):
-            singularity_major_version = int(2)
-            break
-
-    if singularity_major_version == 2:
-        dargs.append('--cap-add=SYS_ADMIN')
-        dargs.append('--cap-add=DAC_OVERRIDE')
-        dargs.append('--cap-add=SETUID')
-        dargs.append('--cap-add=SETGID')
-        dargs.append('--cap-add=SYS_CHROOT')
-    elif singularity_major_version == 3:
-        # Set security options to allow unprivileged singularity to run
-        # The options are secure as long as the system administrator controls the images and does not allow user
-        # code to run as root, and are generally more secure than adding capabilities.
-        #
-        # Enable unshare to be called (which is needed to create namespaces)
-        dargs.append('--security-opt=seccomp=unconfined')
-        # Allow /proc to be mounted in an unprivileged process namespace (as done by singularity exec -p)
-        dargs.append('--security-opt=systempaths=unconfined')
-        # Prevent any privilege escalation (prevents setuid programs from running)
-        dargs.append('--security-opt=no-new-privileges')
-        # In addition, the following option is recommended for allowing unprivileged fuse mounts on kernels that support that.
-        dargs.append('--device=/dev/fuse')
+    # Set security options to allow unprivileged apptainer to run
+    # The options are secure as long as the system administrator controls the images and does not allow user
+    # code to run as root, and are generally more secure than adding capabilities.
+    #
+    # Enable unshare to be called (which is needed to create namespaces)
+    dargs.append('--security-opt=seccomp=unconfined')
+    # Allow /proc to be mounted in an unprivileged process namespace (as done by apptainer exec -p)
+    dargs.append('--security-opt=systempaths=unconfined')
+    # Prevent any privilege escalation (prevents setuid programs from running)
+    dargs.append('--security-opt=no-new-privileges')
+    # In addition, the following option is recommended for allowing unprivileged fuse mounts on kernels that support that.
+    dargs.append('--device=/dev/fuse')
 
     if gateway():
         dargs.append('--label=xrootd-local-gateway=true')
